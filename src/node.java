0b87ecff088e4e22fd80b547fbbb8aa56702ef7f
Trent Blair <tblair22@kentdenver.org>
2021-12-01 14:31:28 -0700

    I coded the classes and tested

--- src/node.java
+++ src/node.java
@@ -1,2 +1,36 @@
-public class node {
+/*
+Trent Blair
+12/1/21
+This is a copy of the node class to use for queue
+ */
+
+public class node <T>{
+
+    private T data;
+    private node <T> next;
+
+    public node(T data) {
+        this.data = data;
+    }
+
+    public node(T data, node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(node<T> next) {
+        this.next = next;
+    }
 }
--- src/queueNode.java
+++ src/queueNode.java
@@ -1,2 +1,43 @@
-public class queueNode {
+/*
+Trent Blair
+12/1/21
+This program implements queue and uses node
+ */
+public class queueNode <T>{
+
+    node<T> first;
+    node<T> last;
+    int size;
+
+    public queueNode() {
+        size = 0;
+    }
+
+    void enqueue(T element){
+        node<T> node = new node<>(element);
+        if(size == 0) first = node;
+        else last.setNext(node);
+        last = node;
+        size++;
+    }
+
+    T dequeue(){
+        T returnVal = first.getData();
+        first = first.getNext();
+        size = Math.max(0, size-1);
+        return returnVal;
+    }
+
+    boolean isEmpty(){
+        return size == 0;
+    }
+
+    int size(){
+        return size;
+    }
+
+    T peek(){
+        return first.getData();
+    }
+
 }
